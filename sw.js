b58667f305382e06ab31795b8064efbce1cbe7fe
JOSE AUGUSTO BARON <jabaron@gmail.com>
2026-03-22 07:32:54 -0300

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'botequim-v10000';
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json',
+    './icon.png',
+    './bar-bg.webp',
+    'https://cdn.tailwindcss.com',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
+];
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+    e.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+        )
+    );
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
+    );
+});
