c8da0ea9e1fba3aa8fb26e396ec7f7a067c8147e
JOSE AUGUSTO BARON <jabaron@gmail.com>
2026-03-25 13:00:49 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
 // ⚠️ Atualize a versão a cada deploy
-const CACHE_VERSION = '1.1.1.10013';
+
+const CACHE_VERSION = '2026.03.25-1430';
 const CACHE_NAME = `botequim-${CACHE_VERSION}`;
 const OFFLINE_URL = './index.html';
 
