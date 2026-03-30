25d14ae16b96ebd0cb6608ca4ae785e1e6f07518
JOSE AUGUSTO BARON <jabaron@gmail.com>
2026-03-29 22:00:44 -0300

    Update CACHE_VERSION for new deployment

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // ⚠️ Atualize a versão a cada deploy
 
-const CACHE_VERSION = '29.03.2026-0001';
+const CACHE_VERSION = '29.03.2026-2200';
 const CACHE_NAME = `botequim-${CACHE_VERSION}`;
 const OFFLINE_URL = './index.html';
 
